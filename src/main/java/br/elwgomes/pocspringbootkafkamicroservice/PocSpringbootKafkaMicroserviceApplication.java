fca4fe14516d2845c130fdb0d87cfb2e347ff388
Leonardo <elwgomes@icloud.com>
2024-08-27 12:49:26 -0300

    initial commit

---
+++ src/main/java/br/elwgomes/pocspringbootkafkamicroservice/PocSpringbootKafkaMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package br.elwgomes.pocspringbootkafkamicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSpringbootKafkaMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSpringbootKafkaMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/elwgomes/pocspringbootkafkamicroservice/PocSpringbootKafkaMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.elwgomes.pocspringbootkafkamicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocSpringbootKafkaMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
